37f187df72d25c88c9a32cf0c4a34601f0227fe5
KasraDaroon <157339209+KasraDaroon@users.noreply.github.com>
2024-06-12 17:42:31 -0400

    Add files via upload
    first python project first.py
    name, age, learning objective.

---
+++ Python_Tutorial/Module_Assessments/First_Assessment/first.py
@@ -0,0 +1,14 @@
+def introduction():
+    name = "Kasra Daroon"
+    age = 27
+    goals = (
+        "I hope that once I finish this project I will be capable of writing simple"
+        "Python programs. I have a decent amount of experience using Python already, "
+        "but it never hurts to continue practising."
+    )
+
+    print(name)
+    print(age)
+    print(goals)
+
+introduction()
