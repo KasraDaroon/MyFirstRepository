e5d4fbb9f1dfed17b4064dc759b082adb15b5bb3
KasraDaroon <157339209+KasraDaroon@users.noreply.github.com>
2024-06-12 20:48:31 -0400

    Add files via upload
    mini project circumference and area of circle calculator

---
+++ main.py
@@ -0,0 +1,11 @@
+radius = float(input("enter radius: "))
+
+pi = 3.1415926
+
+circumference = 2 * pi * radius
+
+area = pi * radius ** 2
+
+print(f"circumference is: {circumference}")
+
+print(f"area is: {area}")
