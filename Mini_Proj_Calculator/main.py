46ec0a0e953b521bb9a8516531f1d44a72a57b9b
KasraDaroon <157339209+KasraDaroon@users.noreply.github.com>
2024-06-12 20:29:30 -0400

    Add files via upload
    mini project create simple calculator

---
+++ Mini_Proj_Calculator/main.py
@@ -0,0 +1,13 @@
+first_number = float(input("Enter 1st number: "))
+
+second_number = float(input("Enter 2nd number: "))
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+print(f"Sum: {sum}")
+print(f"Subtraction: {difference}")
+print(f"Multiplication: {product}")
+print(f"Division: {quotient}")
